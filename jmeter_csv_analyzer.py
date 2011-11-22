253c0f3b499856982711a95986b75a22b2674e0a
Juhani Snellman <juhani.snellman@reaktor.fi>
2011-11-22 14:14:38 +0200

    Fixed the script to support Unicode sample names

--- jmeter_csv_analyzer.py
+++ jmeter_csv_analyzer.py
@@ -8,7 +8,7 @@ import getopt
 class JMeterAnalyzer:
   def __init__(self):
     self.result_file=""
-    self.response_times = {}
+    self.sample_response_times = {}
     self.results_by_sample = {}
     self.sample_count = {}
     self.error_count ={}
@@ -27,17 +27,18 @@ class JMeterAnalyzer:
     self.calculate_summary()
     with open (self.result_file + "_analyzed.csv", "w") as af:
       af.write("Sample Name, Count, Average, 90%, Min, Max, Median, Errors\n")
-      for sample_result in self.results_by_sample:
-        result_string =  str(self.results_by_sample[sample_result])
-        result_string = result_string.lstrip('[')
-        result_string = result_string.replace(']', '\n')
-        af.write(result_string)
+      for sample_name in self.results_by_sample:
+        output_line =  unicode(self.results_by_sample[sample_name])
+        output_line = output_line.lstrip('[')
+        output_line = sample_name + "," + output_line.replace(']', '\n')
+        output_line = output_line.encode('utf-8')
+        af.write(output_line)
 
   def calculate_summary(self):
-    for sample in self.response_times:
-      response_time = map(int, self.response_times[sample])
-      self.results_by_sample[sample] = [sample, self.sample_count[sample], self.average(response_time),self.percentile(response_time, 90),
-                                        min(response_time), max(response_time), self.median(response_time), self.error_count[sample]]
+    for sample_name in self.sample_response_times:
+      response_time = map(int, self.sample_response_times[sample_name])
+      self.results_by_sample[sample_name] = [self.sample_count[sample_name], self.average(response_time),self.percentile(response_time, 90),
+                                        min(response_time), max(response_time), self.median(response_time), self.error_count[sample_name]]
   def median(self, array):
     array.sort()
     return array[len(array)/2]
@@ -50,16 +51,17 @@ class JMeterAnalyzer:
     return array[int(round(len(array)*percent/100))]
 
   def parse_log_file_line(self, line):
-    components = line.split(',')
-    sample_name = components[self.SAMPLE_NAME_INDEX].decode('utf-8')
-    if sample_name in self.response_times:
-      self.response_times[sample_name].append(components[self.RESPONSE_TIME_INDEX])
+    line_components = line.split(',')
+    sample_name = line_components[self.SAMPLE_NAME_INDEX].decode('utf-8')
+    #print sample_name
+    if sample_name in self.sample_response_times:
+      self.sample_response_times[sample_name].append(line_components[self.RESPONSE_TIME_INDEX])
       self.sample_count[sample_name] = self.sample_count[sample_name] +1
-      if components[self.SAMPLE_STATUS_INDEX] != "true": self.error_count[sample_name] += 1
+      if line_components[self.SAMPLE_STATUS_INDEX] != "true": self.error_count[sample_name] += 1
     else:
-      self.response_times[sample_name] = [components[self.RESPONSE_TIME_INDEX]]
+      self.sample_response_times[sample_name] = [line_components[self.RESPONSE_TIME_INDEX]]
       self.sample_count[sample_name] = 1
-      self.error_count[sample_name] = 0 if components[self.SAMPLE_STATUS_INDEX] =="true" else 1
+      self.error_count[sample_name] = 0 if line_components[self.SAMPLE_STATUS_INDEX] =="true" else 1
 
 def print_usage():
     print '''Usage: JMEterAnalylzer.py -f <jmeter-resultfile>
